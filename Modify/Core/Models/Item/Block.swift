7089490b4552ce2a8297ead2af350beea730ccfc
Alex Shevlyakov <sanchosrancho@gmail.com>
2017-09-08 19:12:22 +0300

    Core: User and Block models

--- Modify/Core/Models/Item/Block.swift
+++ Modify/Core/Models/Item/Block.swift
@@ -11,14 +11,16 @@ import CoreLocation
 
 class Block: RealmSwift.Object, Codable {
     @objc dynamic var objectId: String = NSUUID().uuidString
+    @objc dynamic var author: User?
+    @objc dynamic var artifact: Artifact?
     
-    @objc dynamic var latitude: CLLocationDegrees = 0
-    @objc dynamic var longitude: CLLocationDegrees = 0
-    @objc dynamic var altitude: CLLocationDistance = 0
+    @objc dynamic var latitude:  CLLocationDegrees  = 0
+    @objc dynamic var longitude: CLLocationDegrees  = 0
+    @objc dynamic var altitude:  CLLocationDistance = 0
     
     @objc dynamic var horizontalAccuracy: CLLocationAccuracy = -2
-    @objc dynamic var verticalAccuracy: CLLocationAccuracy = -2
-    @objc dynamic var groundDistance: CLLocationDistance = 0
+    @objc dynamic var verticalAccuracy:   CLLocationAccuracy = -2
+    @objc dynamic var groundDistance:     CLLocationDistance = 0
     
 //    @objc dynamic var color: UIColor = UIColor.clear
     @objc dynamic var createdAt: Date?
---
+++ Modify/Core/Models/Item/User.swift
@@ -0,0 +1,43 @@
+//
+//  User.swift
+//  Modify
+//
+//  Created by Alex Shevlyakov on 08/09/2017.
+//  Copyright © 2017 Envent. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import CoreLocation
+
+class User: RealmSwift.Object, Codable {
+    @objc dynamic var objectId: String = NSUUID().uuidString
+    
+    @objc dynamic var deviceId:  String?
+    @objc dynamic var locale:    String?
+    @objc dynamic var pushToken: String?
+    @objc dynamic var platform:  String?
+    
+    let latitude  = RealmOptional<CLLocationDegrees>()
+    let longitude = RealmOptional<CLLocationDegrees>()
+//    let altitude  = RealmOptional<CLLocationDistance>()
+    
+    override static func primaryKey() -> String? {
+        return "objectId"
+    }
+}
+
+extension User {
+    var position: CLLocationCoordinate2D? {
+        set(newPosition) {
+            latitude.value  = newPosition?.latitude
+            longitude.value = newPosition?.longitude
+        }
+        get {
+            guard let lat = latitude.value, let lon = longitude.value else { return nil }
+            return CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        }
+    }
+}
+
+

b88c0f95da9636389f82167f31b5ff6e06b418fc
Alex Shevlyakov <sanchosrancho@gmail.com>
2017-09-23 01:55:24 +0300

    Feat: database queue

--- Modify/Core/Database.swift
+++ Modify/Core/Database.swift
@@ -13,8 +13,37 @@ struct Database {
     
     static private let documents = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
     static private let path = documents + "/modify.realm"
+    static private let queue = DispatchQueue(label: "com.modify.database.queue")
     
     static let realmMain: Realm = {
         return try! Realm(fileURL: URL(fileURLWithPath: Database.path))
     }()
+    
+    static public func doAndSave(realm: Realm, operation: () -> Void) {
+        guard realm.isInWriteTransaction != true else {
+            operation()
+            return
+        }
+        
+        do {
+            try realm.write { operation() }
+        } catch (let error) {
+            print(error)
+        }
+    }
+    
+    static func doInCurrentThread(_ operation: (Realm) -> Void) {
+        do {
+            let realm = try Realm(fileURL: URL(fileURLWithPath: Database.path))
+            operation(realm)
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    static func doInBackground(operation: @escaping (Realm) -> Void) {
+        self.queue.async {
+            self.doInCurrentThread(operation)
+        }
+    }
 }

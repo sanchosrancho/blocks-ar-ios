d90f47445d3cd0e3e4ed4b0e0d41102a453436f7
Alex Shevlyakov <sanchosrancho@gmail.com>
2017-10-04 20:05:21 +0300

    Feat: custom bestLocationEstimate depends on timestamp when we want to freeze location for the scene

--- Modify/Features/AR/ArtifactSceneView.swift
+++ Modify/Features/AR/ArtifactSceneView.swift
@@ -5,8 +5,31 @@
 //  Created by Alex Shevlyakov on 31.08.17.
 //  Copyright © 2017 Envent. All rights reserved.
 //
+import Foundation
 
 class ArtifactSceneView: SceneLocationView {
+    
+    var bestLocationUntilDate: Date?
+    
+    override func bestLocationEstimate() -> SceneLocationEstimate? {
+        guard let untilDate = bestLocationUntilDate else {
+            return super.bestLocationEstimate()
+        }
+        
+        let sortedLocationEstimates = sceneLocationEstimates.sorted(by: {
+            if $0.location.timestamp > untilDate, $1.location.timestamp <= untilDate {
+                return false
+            }
+            
+            if $0.location.horizontalAccuracy == $1.location.horizontalAccuracy {
+                return $0.location.timestamp > $1.location.timestamp
+            }
+
+            return $0.location.horizontalAccuracy < $1.location.horizontalAccuracy
+        })
+
+        return sortedLocationEstimates.first
+    }
     
     public func findNode(by objectId: String) -> ArtifactNode? {
         for node in self.locationNodes {

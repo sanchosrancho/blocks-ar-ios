2a54229aafb0a067432933b430b16b2682da6361
Alex Shevlyakov <sanchosrancho@gmail.com>
2017-10-05 00:46:44 +0300

    Junk: nodes position couldn't be adjusted

--- Modify/Features/AR/ArtifactSceneView.swift
+++ Modify/Features/AR/ArtifactSceneView.swift
@@ -9,27 +9,27 @@ import Foundation
 
 class ArtifactSceneView: SceneLocationView {
     
-    var bestLocationUntilDate: Date?
-    
-    override func bestLocationEstimate() -> SceneLocationEstimate? {
-        guard let untilDate = bestLocationUntilDate else {
-            return super.bestLocationEstimate()
-        }
-        
-        let sortedLocationEstimates = sceneLocationEstimates.sorted(by: {
-            if $0.location.timestamp > untilDate, $1.location.timestamp <= untilDate {
-                return false
-            }
-            
-            if $0.location.horizontalAccuracy == $1.location.horizontalAccuracy {
-                return $0.location.timestamp > $1.location.timestamp
-            }
-
-            return $0.location.horizontalAccuracy < $1.location.horizontalAccuracy
-        })
-
-        return sortedLocationEstimates.first
-    }
+//    var bestLocationUntilDate: Date?
+//    
+//    override func bestLocationEstimate() -> SceneLocationEstimate? {
+//        guard let untilDate = bestLocationUntilDate else {
+//            return super.bestLocationEstimate()
+//        }
+//        
+//        let sortedLocationEstimates = sceneLocationEstimates.sorted(by: {
+//            if $0.location.timestamp > untilDate, $1.location.timestamp <= untilDate {
+//                return false
+//            }
+//            
+//            if $0.location.horizontalAccuracy == $1.location.horizontalAccuracy {
+//                return $0.location.timestamp > $1.location.timestamp
+//            }
+//
+//            return $0.location.horizontalAccuracy < $1.location.horizontalAccuracy
+//        })
+//
+//        return sortedLocationEstimates.first
+//    }
     
     public func findNode(by objectId: String) -> ArtifactNode? {
         for node in self.locationNodes {
